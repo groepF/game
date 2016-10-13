628c5f0c9d7a37350421f3f214a9aee046cf6565
stanvanheumen <cjgheume@student.avans.nl>
2016-10-13 12:00:54 +0200

    You can now use engine in the game project.

--- engine/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Game.h"
-#include <iostream>
-
-int main(int argc, char* args[])
-{
-	Game game("Awesome game", 600, 400);
-
-	return 0;
-}
--- game/main.cpp
+++ game/main.cpp
@@ -1,4 +1,8 @@
+#include "Game.h"
+
 int main(int argc, char* args[])
 {
+	Game game("Awesome game", 600, 400);
+
 	return 0;
 }
