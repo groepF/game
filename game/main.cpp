4c2d408948a9f7f47b8c26e80a6f0d13796c6f0b
Laura de Waard <l.dewaard@student.avans.nl>
2016-10-20 10:41:47 +0200

    Meerdere chunks

--- game/main.cpp
+++ game/main.cpp
@@ -6,8 +6,9 @@ int main(int argc, char* args[])
 
 	AudioController audio{};
 	
-	audio.playSound("../game/sound/fullsample.wav", -1);
-	audio.playSound("../game/sound/sample.wav", 1);
+	audio.playSound("../game/sound/fullsample.wav", -1, 0);
+	audio.playSound("../game/sound/sample.wav", 1, -1);
+	audio.playSound("../game/sound/samplechunk.wav", 1, -1);
 
 	Game game("Awesome game", 600, 400);
 	
