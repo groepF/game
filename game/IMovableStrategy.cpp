7908e285c9b3347d1b727c2b9a15dff44603788f
Diedexx <diede@yoast.com>
2016-10-25 12:22:09 +0200

    re-added filters

--- game/IMovableStrategy.cpp
+++ game/IMovableStrategy.cpp
@@ -1,5 +1,8 @@
 #include "IMovableStrategy.h"
 
+///
+/// Ik geloof dat we deze klasse niet hier moeten maken. In plaats daarvan deze klasse in de engine maken. In de game kunnen er concreteMovableStrategies zijn die van de IMovableStrategy uit te engine overerven.
+///
 IMovableStrategy::IMovableStrategy()
 {
 	//TODO
