378f8fbeb79a54658d37cb8b8c382e92546987d0
stanvanheumen <cjgheume@student.avans.nl>
2017-01-06 11:10:24 +0100

    Saving highscores.

--- game/engine/util/Highscore.cpp
+++ game/engine/util/Highscore.cpp
@@ -29,11 +29,93 @@ void Highscore::save()
 	std::ofstream highscores(FILENAME, std::fstream::out);
 	if (highscores.is_open())
 	{
-		highscores << "This is a line.\n";
-		highscores << "This is another line.\n";
+		highscores << "Most goals in one match:" + std::to_string(mostGoalsInOneMatch) + "\n";
+		highscores << "Win with most score difference:" + std::to_string(scoreDifference) + "\n";
+		highscores << "Most ball posession ever:" + std::to_string(mostBallposession) + "%\n";
+		highscores << "Longest game:" + std::to_string(longestGame) + " seconds\n";
+		highscores << "Fastest goal:" + std::to_string(fastestGoal) + " seconds\n";
+		highscores << "Fastest win:" + std::to_string(fastestWin) + " seconds\n";
 		highscores.close();
 	}
 	else {
 		std::cout << "Unable to open file";
 	}
+}
+
+void Highscore::setMostGoalsInOneMatch(int value)
+{
+	if (value > mostGoalsInOneMatch)
+	{
+		mostGoalsInOneMatch = value;
+	}
+}
+
+void Highscore::setScoreDifference(int value)
+{
+	if (value > scoreDifference)
+	{
+		scoreDifference = value;
+	}
+}
+
+void Highscore::setMostBallposession(int value)
+{
+	if (value > mostBallposession)
+	{
+		mostBallposession = value;
+	}
+}
+
+void Highscore::setLongestGame(int value)
+{
+	if (value > longestGame)
+	{
+		longestGame = value;
+	}
+}
+
+void Highscore::setFastestGoal(int value)
+{
+	if (value > fastestGoal)
+	{
+		fastestGoal = value;
+	}
+}
+
+void Highscore::setFastestWin(int value)
+{
+	if (value > fastestWin)
+	{
+		fastestWin = value;
+	}
+}
+
+int Highscore::getMostGoalsInOneMatch()
+{
+	return mostGoalsInOneMatch;
+}
+
+int Highscore::getScoreDifference()
+{
+	return scoreDifference;
+}
+
+int Highscore::getMostBallPosession()
+{
+	return mostBallposession;
+}
+
+int Highscore::getLongestGame()
+{
+	return longestGame;
+}
+
+int Highscore::getFastestGoal()
+{
+	return fastestGoal;
+}
+
+int Highscore::getFastestWin()
+{
+	return fastestWin;
 }
--- game/engine/util/Highscore.h
+++ game/engine/util/Highscore.h
@@ -8,4 +8,21 @@ class Highscore
 public:
 	static std::vector<std::string> load();
 	static void save();
+
+	static void setMostGoalsInOneMatch(int value);
+	static void setScoreDifference(int value);
+	static void setMostBallposession(int value);
+	static void setLongestGame(int value);
+	static void setFastestGoal(int value);
+	static void setFastestWin(int value);
+
+	static int getMostGoalsInOneMatch();
+	static int getScoreDifference();
+	static int getMostBallPosession();
+	static int getLongestGame();
+	static int getFastestGoal();
+	static int getFastestWin();
+
+private:
+	static int mostGoalsInOneMatch, scoreDifference, mostBallposession, longestGame, fastestGoal, fastestWin;
 };
