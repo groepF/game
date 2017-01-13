42a3b10f6ea9fd89c0c93b63e346cd32d6461514
stanvanheumen <cjgheume@student.avans.nl>
2017-01-13 12:37:06 +0100

    changes

--- game/game/entities/Enemy.cpp
+++ game/game/entities/Enemy.cpp
@@ -3,7 +3,7 @@
 
 #include "../../engine/graphics/render-strategies/RenderDrawableStrategy.h"
 
-Enemy::Enemy(float x, float y) : DrawableEntity(std::make_shared<Sprite>(Sprite("player", 19, 0, 19, 40)), x, y, 0.25f, 0.5f, true)
+Enemy::Enemy(float x, float y) : DrawableEntity(std::make_shared<Sprite>(Sprite("player", 19, 0, 19, 40)), x, y, 0.25f, 0.5f, true, 0, 0, true)
 {
 	this->density = 0.4f;
 	this->restitution = 0.0f;
@@ -18,4 +18,10 @@ Enemy::Enemy(float x, float y) : DrawableEntity(std::make_shared<Sprite>(Sprite(
 void Enemy::hitByPlayer(Ball* b) const
 {
 	b->drop();
+}
+
+float Enemy::getY() const
+{
+
+	return DrawableEntity::getY() + 0.28;
 }
